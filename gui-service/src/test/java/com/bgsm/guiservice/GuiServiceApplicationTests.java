1eddf492b376d2096fa7b8b27d690478f34c505c
robert-er <robert.roza.pl@gmail.com>
2021-01-05 04:35:05 +0100

    add gui-service module

---
+++ gui-service/src/test/java/com/bgsm/guiservice/GuiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgsm.guiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
